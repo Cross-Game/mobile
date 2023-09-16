4be38d30340333f41ff15e5a0bd5b360bec19d73
LeonardoMS07 <leonardo.soares@sptech.school>
2023-09-16 16:03:51 -0300

    wip

---
+++ app/src/main/java/crossgame/android/application/MainActivityListFriend.kt
@@ -0,0 +1,11 @@
+package crossgame.android.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityListFriend : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_friend)
+    }
+}
