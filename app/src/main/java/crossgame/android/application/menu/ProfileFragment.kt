70975849a4a350fead21b2309aa4d0b4ee0dccf6
Gabriel Mello <gabriel.mmello@sptech.school>
2023-10-26 22:53:22 -0300

    Menu feedback BottomSheet finished

--- app/src/main/java/crossgame/android/application/menu/ProfileFragment.kt
+++ app/src/main/java/crossgame/android/application/menu/ProfileFragment.kt
@@ -1,10 +1,14 @@
 package crossgame.android.application.menu
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import crossgame.android.application.FeedbacksActivity
+import crossgame.android.application.databinding.ActivityBsEditProfileBinding
 import crossgame.android.application.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -21,6 +25,22 @@ class ProfileFragment : Fragment() {
             container,
             false
         )
+        binding.btnSettingProfile.setOnClickListener { showBottomSheet() }
         return binding.root
+    }
+
+    private fun showBottomSheet() {
+        val dialog = BottomSheetDialog(binding.root.context)
+
+        val sheetBinding: ActivityBsEditProfileBinding =
+            ActivityBsEditProfileBinding.inflate(layoutInflater, null, false)
+
+        dialog.setContentView(sheetBinding.root)
+        dialog.show()
+        sheetBinding.textVerFeedbacks.setOnClickListener { showFeedbacks() }
+    }
+
+    private fun showFeedbacks() {
+        startActivity(Intent(binding.root.context, FeedbacksActivity::class.java))
     }
 }
