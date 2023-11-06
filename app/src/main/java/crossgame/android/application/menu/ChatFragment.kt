26632738246c4bfde4b394ade4ca02e7874c9b12
LeonardoMS07 <leonardo.soares@sptech.school>
2023-11-06 20:53:38 -0300

    wip

--- app/src/main/java/crossgame/android/application/menu/ChatFragment.kt
+++ app/src/main/java/crossgame/android/application/menu/ChatFragment.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import crossgame.android.application.databinding.FragmentChatBinding
@@ -15,6 +17,9 @@ class ChatFragment : Fragment() {
 
     private lateinit var binding: FragmentChatBinding
     private var friendList: List<Friends> = mutableListOf()
+
+    private lateinit var searchEditText: EditText
+    private lateinit var searchIcon: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +33,19 @@ class ChatFragment : Fragment() {
         )
 
         initializeFriendList()
+
+        searchIcon = binding.searchIconTextView
+        searchEditText = binding.searchEditText
+
+        searchIcon.setOnClickListener {
+            if (searchIcon.visibility == View.VISIBLE) {
+                searchIcon.visibility = View.GONE
+                searchEditText.visibility = View.VISIBLE
+            } else {
+                searchIcon.visibility = View.VISIBLE
+                searchEditText.visibility = View.GONE
+            }
+        }
 
         if (friendList.isNotEmpty()) {
             val recyclerView = binding.recyclerView
@@ -57,7 +75,7 @@ class ChatFragment : Fragment() {
                 "Mello",
                 "Hoje tem UFC",
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlVjfrsqiO8XxWB_iLOsB9mu81mCOjWh-moQ&usqp=CAU"
-            ),Friends(
+            ), Friends(
                 "Junior Patricio",
                 "Bora jogatina",
                 "https://w7.pngwing.com/pngs/227/283/png-transparent-call-break-card-game-android-banduk-game-youtube-android-game-mobile-phones-profile.png"
@@ -86,7 +104,7 @@ class ChatFragment : Fragment() {
                 "Mello",
                 "Hoje tem UFC",
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlVjfrsqiO8XxWB_iLOsB9mu81mCOjWh-moQ&usqp=CAU"
-            ),Friends(
+            ), Friends(
                 "Junior Patricio",
                 "Bora jogatina",
                 "https://w7.pngwing.com/pngs/227/283/png-transparent-call-break-card-game-android-banduk-game-youtube-android-game-mobile-phones-profile.png"
