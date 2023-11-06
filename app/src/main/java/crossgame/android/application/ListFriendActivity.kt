647c1d282b6cfaf0b90705a30ec849460a7c138b
LeonardoMS07 <leonardo.soares@sptech.school>
2023-11-06 20:03:41 -0300

    itens com navbar

--- app/src/main/java/crossgame/android/application/ListFriendActivity.kt
+++ app/src/main/java/crossgame/android/application/ListFriendActivity.kt
@@ -14,60 +14,13 @@ import crossgame.android.ui.adapters.friends.FriendsAdapter
 
 class ListFriendActivity : AppCompatActivity() {
     private lateinit var binding: ActivityListFriendBinding
-    private var friendList: List<Friends> = mutableListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityListFriendBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-//        val searchIcon = findViewById<ImageView>(R.id.searchIconTextView)
-//        val searchEditText = findViewById<EditText>(R.id.searchEditText)
-//        val cancelButton = findViewById<Button>(R.id.cancelButton)
-//
-//        searchIcon.setOnClickListener {
-//            searchEditText.visibility = View.VISIBLE
-//            cancelButton.visibility = View.VISIBLE
-//        }
-//
-//        cancelButton.setOnClickListener {
-//            searchEditText.text.clear()
-//            searchEditText.visibility = View.GONE
-//            cancelButton.visibility = View.GONE
-//        }
-
-
-        initializeFriendList()
-
-        if (friendList.isNotEmpty()) {
-            val recyclerView = binding.recyclerView
-            val friendsAdapter = FriendsAdapter(this, friendList)
-            recyclerView.layoutManager = LinearLayoutManager(this)
-            recyclerView.adapter = friendsAdapter
-        } else {
-            Log.e("ListFriendActivity", "A lista de amigos está vazia.")
-        }
     }
 
-    private fun initializeFriendList() {
-        friendList = mutableListOf(
-            Friends(
-                "Junior Patricio",
-                "Bora jogatina",
-                "https://w7.pngwing.com/pngs/227/283/png-transparent-call-break-card-game-android-banduk-game-youtube-android-game-mobile-phones-profile.png"
-            ),
-            Friends(
-                "China",
-                "Partiu Sekiro",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwudakIrwTEVtZMpYd8zO6NWzaIdpg6hkcJA&usqp=CAU"
-            ),
-            Friends(
-                "Mello",
-                "Hoje tem UFC",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlVjfrsqiO8XxWB_iLOsB9mu81mCOjWh-moQ&usqp=CAU"
-            )
-        )
-        // Log para verificar a lista
-        Log.d("ListFriendActivity", "Amigos: $friendList")
-    }
+
 }
