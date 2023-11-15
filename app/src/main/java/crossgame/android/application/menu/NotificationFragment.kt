803536606e48bf89b3cdf181902117657915f654
Matheus <matheus.patricio@sptech.school>
2023-11-15 16:30:41 -0300

    add verification by notification type

--- app/src/main/java/crossgame/android/application/menu/NotificationFragment.kt
+++ app/src/main/java/crossgame/android/application/menu/NotificationFragment.kt
@@ -3,6 +3,7 @@ package crossgame.android.application.menu
 import NotificationAdapter
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +12,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import crossgame.android.application.databinding.FragmentNotificationBinding
 import crossgame.android.domain.httpClient.Rest
+import crossgame.android.domain.models.friends.Friends
 import crossgame.android.domain.models.notifications.Notification
 import crossgame.android.domain.models.notifications.NotificationState
 import crossgame.android.domain.models.notifications.NotificationType
+import crossgame.android.domain.models.user.UserList
 import crossgame.android.service.AutenticationUser
 import crossgame.android.service.NotificationService
+import crossgame.android.service.UserFriendService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -44,10 +48,9 @@ class NotificationFragment : Fragment() {
             false
         )
 
-        CoroutineScope(Dispatchers.Main).launch {
-            notificationList = fetchNotifications()
+            notificationList = retrieveNotifications()
 
-        }
+
         val notificationAdapter = NotificationAdapter(requireContext(), notificationList)
         recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -56,20 +59,30 @@ class NotificationFragment : Fragment() {
         return binding.root
     }
 
-    private suspend fun fetchNotifications(): List<Notification> {
-        val sharedPreferences = requireActivity().getSharedPreferences("MinhasPreferencias", Context.MODE_PRIVATE)
+
+    private fun retrieveNotifications() : List<Notification> {
+        val sharedPreferences =
+            requireActivity().getSharedPreferences("MinhasPreferencias", Context.MODE_PRIVATE)
         val userId = sharedPreferences.getInt("id", 0).toLong()
+
         val rest = Rest.getInstance(requireActivity())
+        val service = rest.create(NotificationService::class.java)
 
-        return withContext(Dispatchers.IO) {
-            val response = rest.create(NotificationService::class.java).retrieveNotifications(1).execute()
-            if (response.isSuccessful) {
-                return@withContext response.body() ?: emptyList()
-            } else {
-                emptyList()
+        service.retrieveNotifications(userId).enqueue(object : Callback<List<Notification>> {
+            override fun onResponse(
+                call: Call<List<Notification>>,
+                response: Response<List<Notification>>
+            ) {
+                if (response.isSuccessful){
+                    notificationList = response.body()!!
+                    }
             }
-        }
+
+            override fun onFailure(call: Call<List<Notification>>, t: Throwable) {
+                println("Erro")
+            }
+
+        })
+        return  notificationList
     }
-
-
 }
