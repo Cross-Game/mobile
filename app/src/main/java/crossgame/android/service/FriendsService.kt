855a30f0324d33405a427ddfad9cb14b29786850
Matheus <matheus.patricio@sptech.school>
2023-11-17 10:22:39 -0300

    att FriendsService

--- app/src/main/java/crossgame/android/service/FriendsService.kt
+++ app/src/main/java/crossgame/android/service/FriendsService.kt
@@ -1,5 +1,6 @@
 package crossgame.android.service
 
+import crossgame.android.domain.models.friends.FriendAdd
 import crossgame.android.domain.models.friends.Friends
 import crossgame.android.domain.models.users.UserFriend
 import retrofit2.Call
@@ -17,4 +18,9 @@ interface FriendsService {
 
     @POST("friends/{userId}")
     fun addFriendToAnUser(@Path("userId") userId: Long, @Body friendAdd : FriendAdd): Call<Unit>
+    @PATCH("friends/confirming-friend-request/{userId}/{friendUsername}")
+    fun confirmFriendRequest(@Path("userId") userId: Long,@Path("friendUsername") friendUsername: String):Call<UserFriend>
+    @DELETE("friends/declining-friend-request/{userId}/{friendUsername}")
+    fun decliningFriendRequest(@Path("userId") userId: Long,@Path("friendUsername") friendUsername: String):Call<Void>
+
 }
