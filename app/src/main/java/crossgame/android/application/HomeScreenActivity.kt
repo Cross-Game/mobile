2554510d3a9fcb63d8a9443aac66f7221d850370
Gabriel Mello <gabriel.mmello@sptech.school>
2023-11-26 15:08:29 -0300

    biometria 100% funcionando

--- app/src/main/java/crossgame/android/application/HomeScreenActivity.kt
+++ app/src/main/java/crossgame/android/application/HomeScreenActivity.kt
@@ -3,18 +3,23 @@ package crossgame.android.application
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
 import crossgame.android.application.databinding.ActivityHomeScreenBinding
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import crossgame.android.infra.BiometricHelper
+import java.util.concurrent.Executor
 
 class HomeScreenActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeScreenBinding
+    private lateinit var rootView: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.btnHomeCadastrar.setOnClickListener { telaCadastro() }
         binding.btnEntrar.setOnClickListener { telaEntrar() }
     }
@@ -23,8 +28,30 @@ class HomeScreenActivity: AppCompatActivity() {
         super.onStart()
         val sharedPreferences = getSharedPreferences("MinhasPreferencias", Context.MODE_PRIVATE)
         val username = sharedPreferences.getString("username", "")
-        if (username != "") {
-            redirectToMatch()
+        if (BiometricHelper.isBiometricAvailable(this)) {
+
+            val executor: Executor = ContextCompat.getMainExecutor(this)
+
+            val biometricPrompt = BiometricPrompt(this, executor,
+                object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        if (username != "") {
+                            redirectToMatch()
+                        } else {
+                            val snackbar = Snackbar.make(rootView, "Usuario não logado !", Snackbar.LENGTH_SHORT)
+                        }
+                        super.onAuthenticationSucceeded(result)
+                    }
+                })
+
+            // Informações apresentadas no momento da autenticação
+            val info: BiometricPrompt.PromptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("CrossGame")
+                .setDescription("Desbloqueie seu celular")
+                .setNegativeButtonText("Cancelar")
+                .build()
+
+            biometricPrompt.authenticate(info)
         }
     }
 
